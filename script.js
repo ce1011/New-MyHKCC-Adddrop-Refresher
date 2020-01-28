fcfe2bcb39c84ef97cff1b65c63ca9f1f0c00874
Parco Au-Yeung <paco20071001@gmail.com>
2020-01-28 07:39:20 +0000

    Popup notification when any 1 subject is avalible.

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         New MyHKCC Add/Drop Toolkit
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  Fuck 0 Vacancy!
+// @version      0.1
+// @description  try to take over the world!
 // @author       You
 // @include					http*://www*.polyu.edu.hk/myhkcc_new/*
 // @include					http://127.0.0.1/index.html
-// @include                 file:///C:/Users/ce/Desktop/New%20folder/my.HKCC%20Student%20Portal.html
 // @grant        none
 // @require https://code.jquery.com/jquery-3.4.1.slim.js
 // @grant       unsafeWindow
@@ -19,6 +18,7 @@ $(document).ready(function() {
 
   var subjectName = []
   var vacancy = []
+  var notificationStatus = []
 
   var toolkit = (window.toolkit = {})
 
@@ -27,7 +27,7 @@ $(document).ready(function() {
       "<div class='item'<span>ADD/Drop Toolkit</span><div class='menu'><button onclick='Notification.requestPermission()'>Grant Permission for Notification</button><p>Type 'toolkit.startRefresh()' to start automatic refresh</p><p>Type 'toolkit.stopRefresh' to stop refresh</div></div>"
     ).insertBefore(
       "#sidebarPushable > div.pusher.purify_2DCQA > div > div.purify_9yySi > div.purify_13WEs.purify_3eNdw > div > div:nth-child(2)"
-    ); // Select top of sidebar and add permission grant at the sidebar
+    );
   }, 100);
 
   toolkit.startRefresh = function() {
@@ -36,7 +36,7 @@ $(document).ready(function() {
       document
         .querySelector(
           "body > div:nth-child(7) > div > div:nth-child(3) > button.ui.button.purify_3Vjpt"
-        ) // Click refresh
+        )
         .click();
       n++;
       toolkit.updateSubjectList();
@@ -53,12 +53,26 @@ $(document).ready(function() {
 
   toolkit.updateSubjectList = function(){
     $("body > div:nth-child(7) > div > div:nth-child(2)")[0].childNodes.forEach(function (subjectNo, i){
-        subjectName[i] = subjectNo.children[0].innerText; // Fetch add/drop vacancy data from the vacancy refresh list
-        vacancy[i] = subjectNo.children[1].innerText ]
+        subjectName[i] = subjectNo.children[0].innerText;
+        vacancy[i] = subjectNo.children[1].innerText
         if(vacancy[i] > 0){
-
+            if(notificationStatus[i] != 1){
+              notificationStatus[i] = 1
+              showNotification(subjectNo.children[0].innerText)
+            }
             console.log("%c Subject: " + subjectNo.children[0].innerText + "\nVancancy:" + subjectNo.children[1].innerText, "color: green; font-weight: bold;")
         }
     })
+
+    function showNotification(subject){
+      var notification = new Notification(subject + " is available. Please stop refresh and submit your add/drop request as fast as possible.")
+
+      notification.onclick = function(event) {
+        event.preventDefault()
+        toolkit.stopRefresh()
+      }
+    }
+
+
   }
 });
